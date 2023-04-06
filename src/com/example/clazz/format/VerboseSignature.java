43c8bcb08934d16926b9b2acf8d76ffe86e38ff4
lovecluo <lovecluo@gmail.com>
2023-04-06 16:26:13 +0800

    Signature 测试代码

--- src/com/example/clazz/format/VerboseSignature.java
+++ src/com/example/clazz/format/VerboseSignature.java
@@ -1,7 +1,27 @@
 package com.example.clazz.format;
 
-public class VerboseSignature <T> {
+import java.util.List;
+import java.util.Map;
+
+public class VerboseSignature<T> {
     public T parameterizedVerbose;
+    Map<String, Integer> map;
+
+    public void setupSuperParams(List<? super ClassFormat> data) {
+
+    }
+
+    public void setupExtendsParams(List<? extends ClassFormat> data) {
+
+    }
+
+    public void setupExtendsT(List<? extends T> data) {
+
+    }
+
+    public void setup(List<?> data) {
+        
+    }
 }
 
 
