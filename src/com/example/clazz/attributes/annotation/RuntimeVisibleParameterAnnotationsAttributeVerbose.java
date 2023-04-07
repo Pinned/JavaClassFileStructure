34cced4e49eedf5ca589fe6e6834142c2ec3e5d1
lovecluo <lovecluo@gmail.com>
2023-04-07 13:42:21 +0800

    修复方法关联错误

--- src/com/example/clazz/attributes/annotation/RuntimeVisibleParameterAnnotationsAttributeVerbose.java
+++ src/com/example/clazz/attributes/annotation/RuntimeVisibleParameterAnnotationsAttributeVerbose.java
@@ -2,10 +2,7 @@ package com.example.clazz.attributes.annotation;
 
 import com.example.clazz.attributes.AttributeVerbose;
 import com.example.clazz.attributes.annotation.AnnotationElement;
-import com.example.clazz.dot.ClassDot;
-import com.example.clazz.dot.DotItem;
-import com.example.clazz.dot.DotShape;
-import com.example.clazz.dot.DotStyle;
+import com.example.clazz.dot.*;
 
 import java.io.DataInputStream;
 import java.io.IOException;
@@ -28,26 +25,29 @@ public class RuntimeVisibleParameterAnnotationsAttributeVerbose extends Attribut
     @Override
     public DotItem createDotItem(ClassDot classDot, DotItem parent, int index) {
         DotItem superItem = super.createDotItem(classDot, parent, index);
-
-        DotItem paramItem = new DotItem("params_number", String.valueOf(numParameters))
+        DotItem paramItem = new DotItem("params_number", String.valueOf(numParameters) + "个\\n参数")
                 .parent(superItem).shape(DotShape.CIRCLE).style(DotStyle.DASHED);
         superItem.addChild("", paramItem);
         for (int i = 0; i < numParameters; i++) {
-            createAnnotationDotItem(classDot, paramItem, i, parameterAnnotations.get(i));
+            DotItem item = createAnnotationDotItem(classDot, paramItem, i, parameterAnnotations.get(i));
+            paramItem.addChild("", item);
         }
 
 
         return superItem;
     }
 
-    private void createAnnotationDotItem(ClassDot classDot, DotItem parent, int index, AnnotationElementArray array) {
-        DotItem numItem = new DotItem("number_" + index, String.valueOf(array.numAnnotations))
-                .parent(parent);
-        parent.addChild("num", numItem);
+    private DotItem createAnnotationDotItem(ClassDot classDot, DotItem parent, int index, AnnotationElementArray array) {
+        ArrayDotItem emptyItem = new ArrayDotItem("annotation", index, "")
+                .parent(parent).shape(DotShape.CIRCLE).style(DotStyle.DASHED);
+        DotItem numItem = new DotItem("number", String.valueOf(array.numAnnotations))
+                .parent(emptyItem);
+        emptyItem.addChild("num", numItem);
         for (int i = 0; i < array.numAnnotations; i++) {
-            DotItem item = array.elements[i].createDotItem(classDot, parent, i);
-            parent.addChild("", item);
+            DotItem item = array.elements[i].createDotItem(classDot, emptyItem, i);
+            emptyItem.addChild("", item);
         }
+        return emptyItem;
     }
 
 }
