747d9fda4a97a716ca4d74b56f441090abbe2d51
lovecluo <lovecluo@gmail.com>
2023-04-23 19:18:02 +0800

    Fix MethodHandlerKind

--- src/com/example/clazz/constant/MethodHandleConstantVerbose.java
+++ src/com/example/clazz/constant/MethodHandleConstantVerbose.java
@@ -21,13 +21,38 @@ class MethodHandleConstantVerbose implements ConstantVerbose {
         System.out.println("#" + index + " = MethodHandle\t" + methodHandleKind + ".#" + methodHandleIndex);
     }
 
+
     @Override
     public ConstantArrayDotItem createDotItem(int index, ClassDot classDot) {
-        ConstantArrayDotItem item = new ConstantArrayDotItem(index, "MethodHandle(" + methodHandleKind + ")");
+        ConstantArrayDotItem item = new ConstantArrayDotItem(index, "MethodHandle(" + getMethodHandleKindReadableValue() + ")");
         item.constant = this;
         return item;
     }
 
+    public String getMethodHandleKindReadableValue() {
+        switch (methodHandleKind) {
+            case 1:
+                return "REF_getField";
+            case 2:
+                return "REF_getStatic";
+            case 3:
+                return "REF_putField";
+            case 4:
+                return "REF_putStatic";
+            case 5:
+                return "REF_invokeVirtual";
+            case 6:
+                return "REF_invokeStatic";
+            case 7:
+                return "REF_invokeSpecial";
+            case 8:
+                return "REF_newInvokeSpecial";
+            case 9:
+                return "REF_invokeInterface";
+            default:
+                return "Unknown";
+        }
+    }
     @Override
     public void updateConstantIndex(DotItem item, ClassDot classDot) {
         item.addChild("methodHandle", classDot.getConstantItem(methodHandleIndex));
